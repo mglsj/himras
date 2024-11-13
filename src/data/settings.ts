751794037fc640af68dc736532aeef719a79987c
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-11-13 15:32:39 +0530

    init

---
+++ src/data/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+	site: "https://himras.in",
+	name: "HIMRAS",
+	title: "The Himras Store",
+	description: "Bringing you closer to nature.",
+};
---
+++ src/data/socials.ts
@@ -0,0 +1,7 @@
+export const socials = [
+	{
+		title: "Instagram",
+		icon: "mdi:instagram",
+		url: "https://www.instagram.com/the_himras_store/",
+	},
+];
